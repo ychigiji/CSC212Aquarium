1a8a0972ad8112079c07ad5a1ee9918381ebdc7b
nyashayolanda <nyashayolanda@131.229.138.29>
2019-09-13 14:23:31 -0400

    Working on the lab. I created a Fish class.

---
+++ src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
@@ -0,0 +1,19 @@
+package edu.smith.cs.csc212.aquarium;
+import java.awt.color;
+import java.awt. Graphics2D;
+
+
+public class Fish {
+	// color of the fish
+	Color color;
+	// position of the fish
+	int x;
+	int y;
+	//construct a fish at a position with color
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
